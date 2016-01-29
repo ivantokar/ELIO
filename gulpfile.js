0bffcd27a702ff8ec1dd5bbce8aae344ab3c45ef
Ivan Tokar <ivan.yu.tokar@yandex.ru>
2016-01-29 20:58:10 +0200

    Jade template, add styles for typography (colors: light, red, green, blue, orange. font styles: italic, bold, underline. tag a styles)

--- gulpfile.js
+++ gulpfile.js
@@ -2,60 +2,47 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	minifycss = require('gulp-minify-css'),
 	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
-	notify = require('gulp-notify'),
 	livereload = require('gulp-livereload'),
-	uglify = require('gulp-uglify');
+	jade = require('gulp-jade');
 
-gulp.task('default', function() {
-	gulp.run('watch', 'copy', 'styles', 'scripts');
+
+gulp.task('watcher', ['jade', 'styles', 'scripts'], function() {
+	livereload.listen();
+	gulp.watch('sass/**/*.scss', ['styles', 'sources']);
+	gulp.watch('jade/**/*.jade', ['jade']);
+	gulp.watch('scripts/**/*.js', ['scripts', 'sources']);
+});
+
+gulp.task('jade', function () {
+	return gulp.src('jade/**/*.jade')
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest('assets/html/'))
+		.pipe(livereload());
 });
 
 gulp.task('styles', function () {
-	return gulp.src(['./sass/**/*.scss', './bower_components/font-awesome/css/font-awesome.min.css'])
+	return gulp.src(['./sass/**/*.scss'])
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./assets/css/'))
 		.pipe(concat('all.css'))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(minifycss())
 		.pipe(gulp.dest('./assets/css'))
-		.pipe(livereload())
-		.pipe(notify({ message: 'Styles task complete' }));
-});
-
-gulp.task('copy', function() {
-	gulp.src(['bower_components/font-awesome/fonts/**'])
-		.pipe(gulp.dest('./assets/fonts/'));
+		.pipe(livereload());
 });
 
 gulp.task('scripts', function() {
-	return gulp.src(['./bower_components/jquery/dist/jquery.min.js',
-					// './bower_components/waypoints/lib/jquery.waypoints.min.js',
-					// './bower_components/jquery-smooth-scroll/jquery.smooth-scroll.min.js',
-					// './bower_components/parallax.js/parallax.min.js',
-					// './scripts/src/jquery.elio.animation.js',
-					// './scripts/src/functions.js',
-					// './scripts/src/main.js'
-					])
+	return gulp.src(['./scripts/app.js'])
 		.pipe(concat('all.js'))
 		.pipe(gulp.dest('./assets/js/'))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(uglify())
 		.pipe(gulp.dest('./assets/js/'))
-		.pipe(livereload())
-		.pipe(notify({ message: 'Scripts task complete' }));
+		.pipe(livereload());
 });
 
-gulp.task('watch', function() {
-	livereload.listen();
-
-	gulp.watch('sass/**/*.scss', function(event) {
-		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-		gulp.run('styles');
-	});
-
-	gulp.watch('scripts/**/*.js', function(event) {
-		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-		gulp.run('scripts');
-	});
-});
+gulp.task('default', ['watcher']);
